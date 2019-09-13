93aefca7879bba90ba94646f07ef4f93206ab203
cryten <cryten101@gmail.com>
2019-09-13 13:19:51 +0200

    Setup / Fixed unity remote 5 bug

---
+++ Game/Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public bool isFlat = true;
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        if (isFlat)
+        {
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+
+// Adds force to rigidbody dependent on acceleration
+        _rigidbody.AddForce(tilt);
+    }
+}
