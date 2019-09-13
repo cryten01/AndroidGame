ffe4e1e910f12d9b3eab52ea7c87690e5344cf84
cryten <cryten101@gmail.com>
2019-09-13 15:29:24 +0200

    Acc / Added basic accelerometer functions

---
+++ Game/Assets/Scripts/GyroFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     GyroManager.Instance.enableGyro();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = GyroManager.Instance.GyroRotation * new Quaternion(0, 0, 1, 0);
+    }
+}
---
+++ Game/Assets/Scripts/GyroManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroManager : MonoBehaviour
+{
+    private static GyroManager instance;
+
+    public static GyroManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GyroManager>();
+
+                if (instance == null)
+                {
+                    instance = new GameObject("Spawned Gyromanager", typeof(GyroManager)).GetComponent<GyroManager>();
+                }
+            }
+
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+
+
+    private Gyroscope _gyro;
+    private Quaternion _gyroRotation;
+    private bool gyroActive;
+
+    public void enableGyro()
+    {
+        if (gyroActive)
+        {
+            return;
+        }
+
+        if (SystemInfo.supportsGyroscope)
+        {
+            _gyro = Input.gyro;
+            _gyro.enabled = true;      
+            gyroActive = _gyro.enabled;
+        }
+        else
+        {
+            Debug.Log("Gyro is not supported on this device");
+        }
+    }
+
+    private void Update()
+    {
+        if (gyroActive)
+        {
+            _gyroRotation = _gyro.attitude;
+//            Debug.Log("Rotation: " + _gyroRotation);
+        }
+    }
+
+    public Quaternion GyroRotation => _gyroRotation;
+}
