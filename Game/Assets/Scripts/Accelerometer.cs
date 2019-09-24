726495d086bc537c705416e85bdd239117dc3ca5
cryten <cryten101@gmail.com>
2019-09-24 21:11:56 +0200

    Added Platform & Ball movement via accelerometer

--- Game/Assets/Scripts/Accelerometer.cs
+++ Game/Assets/Scripts/Accelerometer.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Accelerometer : MonoBehaviour
 {
-    public bool isFlat = true;
+    // External game objects
+    public GameObject platform;
+    public TextMeshProUGUI accValues;
+    
+    // Public params
+    public bool platformMode = true;
+    public float moveSpeed = 50;
+    
+    // Filtered values of the accelerometer
+    private float xFilt = 0.0f;
+    private float yFilt = 0.0f;
+   
     private Rigidbody _rigidbody;
+    
+    // For lerp test only
+    private Vector3 newPosition = new Vector3(0, 0, 0);
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +32,49 @@ public class Accelerometer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 tilt = Input.acceleration;
-        
-        if (isFlat)
+        // Filters acceleration values for smoother movements
+        // y change need because device is held flat
+        lowPassFilter(Input.acceleration.x, Input.acceleration.y, 0.1f);
+
+        // Debug: diplays current xFilt and yFilt on UI
+        accValues.text = "x: " + xFilt + " z: " + yFilt;
+    }
+
+    private void FixedUpdate()
+    {
+        if (platformMode)
         {
-            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+            platform.transform.localRotation = Quaternion.Euler(yFilt * moveSpeed, 0, -xFilt * moveSpeed);
+//            Quaternion.Lerp(platform.transform.localRotation, Quaternion.Euler(yFilt, 0, -xFilt), Time.deltaTime);
+        }
+        else
+        {
+            _rigidbody.AddForce(Input.acceleration.x * moveSpeed, 0, Input.acceleration.y * moveSpeed);
+        }
+    }
+
+
+    private void lerpTest(GameObject target)
+    {
+        Vector3 posA = new Vector3(0, 0, 0);
+        Vector3 posB = new Vector3(0, -4, 0);
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            newPosition = posA;
         }
 
-        // Adds force to rigidbody dependent on acceleration
-        _rigidbody.AddForce(tilt);
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            newPosition = posB;
+        }
+
+        target.transform.position = Vector3.Lerp(target.transform.position, newPosition, Time.deltaTime);
+    }
+
+    private void lowPassFilter(float accX, float accY, float alpha)
+    {
+        xFilt = alpha * accX + (1 - alpha) * xFilt;
+        yFilt = alpha * accY + (1 - alpha) * yFilt;
     }
 }
--- Game/Assets/Scripts/GameManager.cs
+++ Game/Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
+{    
     // Start is called before the first frame update
     void Start()
     {
